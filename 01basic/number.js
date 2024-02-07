6bc25341ed880e536ec82f56e42aa96fb62af131
gaurav992000 <144047956+gaurav992000@users.noreply.github.com>
2024-02-07 16:25:37 +0000

    COMMIT

---
+++ 01basic/number.js
@@ -0,0 +1,14 @@
+const balancee=400
+console.log(balancee)
+// const balance=new Number(400)
+const balance=400
+console.log(balance)
+console.log(balance.toString())
+console.log(typeof(balance))
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+const hundered=1000000
+console.log(hundered.toLocaleString())
+// DEFAULT O/P=>1,000,000
+console.log(hundered.toLocaleString('en-IN'))
+// O/P=>10,00,000
