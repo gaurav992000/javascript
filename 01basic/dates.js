933308d622d1eeee56c9d3333b61790d0c193cfd
gaurav992000 <144047956+gaurav992000@users.noreply.github.com>
2024-02-08 08:19:17 +0000

    commit

---
+++ 01basic/dates.js
@@ -0,0 +1,5 @@
+const date=new Date()
+console.log(date)
+console.log(date.toDateString());
+console.log(date.toLocaleString());
+console.log(typeof date)
--- 01basic/number.js
+++ 01basic/number.js
@@ -1,14 +1,24 @@
-const balancee=400
-console.log(balancee)
+// const balancee=400
+// console.log(balancee)
 // const balance=new Number(400)
-const balance=400
-console.log(balance)
-console.log(balance.toString())
-console.log(typeof(balance))
-console.log(balance.toString().length)
-console.log(balance.toFixed(1))
-const hundered=1000000
-console.log(hundered.toLocaleString())
+// const balance=400
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(typeof(balance))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+// const hundered=1000000
+// console.log(hundered.toLocaleString())
 // DEFAULT O/P=>1,000,000
-console.log(hundered.toLocaleString('en-IN'))
+// console.log(hundered.toLocaleString('en-IN'))
 // O/P=>10,00,000
+// +------------------------------------------++++
+// console.log(Math)
+// console.log(Math.abs(-4))
+// Math.random value always lies between 0 and 1
+console.log(Math.random())
+// console.log(Math.random()*10)=>8.788978215332381
+// console.log((Math.random()*10)+1)=>8
+const min =10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
